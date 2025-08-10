5fac93a8202318ee4ed7e9fefc1b96084f3d216c
dz33 <derekzhao33@gmail.com>
2025-08-09 22:11:26 -0700

    Add createGrid and createColumn functions

---
+++ main.js
@@ -0,0 +1,33 @@
+const container = document.querySelector('.container');
+
+function createGrid(dimension) {
+  for (let i = 0; i < dimension; i++) {
+    const column = createcolumn(dimension)
+    container.appendChild(column);
+
+    column.offsetHeight = container.offsetHeight / dimension;
+    column.offsetWidth = container.offsetWidth / dimension;
+  }
+}
+
+function createcolumn(dimension) {
+  const column = document.createElement('div');
+  column.classList.add('column');
+
+  column.style.height = container.offsetHeight;
+  column.style.width = container.offsetWidth / dimension;
+
+  for (let i = 0; i < dimension; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+
+    column.appendChild(box);
+
+    box.style.width = container.offsetWidth / (dimension ** 2);
+    box.style.height = container.offsetHeight / (dimension ** 2);
+  }
+
+  return column;
+}
+
+createGrid(16);c
